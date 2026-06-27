6cfd9c622a59f3a1699a858d52bb2a1bf0438b57
destripador2000 <davidhumbertop503@gmail.com>
2026-06-27 14:32:23 -0600

    Feat: Creación de métodos para Goals

---
+++ backend/src/domain/repositories/IGoalRepository.ts
@@ -0,0 +1,8 @@
+import { Goal } from '../entities/Goal';
+
+// Métodos para crear, encontrar y mostrar Goals
+export interface IGoalRepository {
+  create(goal: Goal): Promise<void>;
+  findById(id: string): Promise<Goal | null>;
+  findAll(userId: string): Promise<Goal[]>   
+}

eb7472f7b0116a28e8536fac40301ee7fc48ef9f
destripador2000 <davidhumbertop503@gmail.com>
2026-06-28 08:35:57 -0600

    Feat: Creación de controlador para la creación de Goal

---
+++ backend/src/infrastructure/controllers/GoalController.ts
@@ -0,0 +1,20 @@
+import { type Request, type Response } from 'express';
+import { CreateGoalUseCase } from '../../application/use-cases/CreateGoalUseCase';
+
+export class GoalController {
+  // Inyectamos el Caso de Uso en el controlador
+  constructor(private readonly createGoalUseCase: CreateGoalUseCase) {}
+
+  // Usamos una arrow function para no perder el contexto de 'this'
+  public createGoal = async (req: Request, res: Response): Promise<void> => {
+    try {
+      const {title, totalAmount, currency, userId} = req.body
+      const newGoal = await this.createGoalUseCase.execute({title, totalAmount, currency, userId})
+      res.status(201).json(newGoal)
+
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: 'Error interno del servidor al crear la meta' });
+    }
+  };
+}

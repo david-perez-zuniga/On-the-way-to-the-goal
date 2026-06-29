f448e6bf08cd46c5f9cab8abefbfd525efeddaca
destripador2000 <davidhumbertop503@gmail.com>
2026-06-29 07:46:18 -0600

    Feat: Instanceando pool de conexiones e inyectando conexión de la base de datos

--- backend/src/infrastructure/db/prisma.ts
+++ backend/src/infrastructure/db/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaPg } from '@prisma/adapter-pg';
 import { PrismaClient } from './index';
+import { Pool } from 'pg';
 
-const adapter = new PrismaPg();
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString })
+
+const adapter = new PrismaPg(pool);
+
 export const prisma = new PrismaClient({ adapter });

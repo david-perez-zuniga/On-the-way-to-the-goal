b47514ee2ac996da02036d8f2d7f09cc2fe37b4d
destripador2000 <davidhumbertop503@gmail.com>
2026-06-29 15:34:09 -0600

    Feat: Implementación de ruta /dashboard en App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import LandingPage from './features/landing/components/LandingPage'
 import RegisterPage from './features/registration/components/RegisterPage'
 import LoginPage from './features/login/components/LoginPage'
+import DashboardPage from './features/dashboard/components/DashboardPage'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/registro" element={<RegisterPage />} />
       <Route path="/iniciar-sesion" element={<LoginPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
     </Routes>
   )
 }

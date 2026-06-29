29857bee0018eaee62b9e7c42a774bbd2aaf66cd
destripador2000 <davidhumbertop503@gmail.com>
2026-06-29 14:12:11 -0600

    feat: add /iniciar-sesion route to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom'
 import LandingPage from './features/landing/components/LandingPage'
 import RegisterPage from './features/registration/components/RegisterPage'
+import LoginPage from './features/login/components/LoginPage'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LandingPage />} />
       <Route path="/registro" element={<RegisterPage />} />
+      <Route path="/iniciar-sesion" element={<LoginPage />} />
     </Routes>
   )
 }

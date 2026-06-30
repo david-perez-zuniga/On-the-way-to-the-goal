37a48ac428dd81759e73ff592b9507de6cd0ccdd
destripador2000 <davidhumbertop503@gmail.com>
2026-06-30 10:41:12 -0600

    Feat: Agregando ruta de login al servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import 'dotenv/config';
 import express from 'express';
 import { goalRoutes } from './infrastructure/routes/goalRoutes';
 import { userRoutes } from './infrastructure/routes/userRoutes';
+import { loginRoutes } from './infrastructure/routes/loginRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -11,6 +12,7 @@ app.use(express.json());
 
 app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
+app.use('/api/login', loginRoutes)
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: '¡Servidor de Camino a la Meta funcionando!' });

43cba85ecb6b5420285b00e8d992e240ab39a847
destripador2000 <davidhumbertop503@gmail.com>
2026-06-30 10:41:12 -0600

    Feat: Implementando caso de uso de login con JWT

---
+++ backend/src/application/use-cases/LoginUseCase.ts
@@ -0,0 +1,30 @@
+import { type IUserRepository } from "../../domain/repositories/IUserRepository";
+import bcrypt from 'bcrypt'
+import jwt from "jsonwebtoken"
+
+export interface LoginUserDTO{
+  email: string;
+  password: string;
+}
+
+export class LoginUseCase{
+  constructor(private readonly userRepository: IUserRepository){}
+
+  public async execute(data: LoginUserDTO): Promise<string>{
+    const user = await this.userRepository.findByEmail(data.email);
+
+    if (user === null){
+      throw new Error('Credenciales incorrectas')
+    }
+
+    const isPasswordValid = await bcrypt.compare(data.password, user.password)
+    if (isPasswordValid == false){
+      throw new Error('Credenciales incorrectas')
+    }
+
+    const token = jwt.sign({ userId: user.id}, 'SECRETO', {expiresIn: '30min'})
+
+    return token
+    
+  }
+}

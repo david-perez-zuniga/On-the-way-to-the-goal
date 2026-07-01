be9aa0d8d2918eb711271ff5782253b6a6fdbc3f
destripador2000 <davidhumbertop503@gmail.com>
2026-07-01 15:05:49 -0600

    Feat: Creación del método findAll que permite mostrar las metas de un usuario

--- backend/src/domain/entities/Goal.ts
+++ backend/src/domain/entities/Goal.ts
@@ -1,26 +1,15 @@
+import type { Prisma } from "../../infrastructure/db";
+
 // Contrato del modelo Goal
 export class Goal {
   constructor(
     public readonly id: string,
     public title: string,
-    public totalAmount: number,
-    public currentAmount: number, 
+    public totalAmount: Prisma.Decimal,
     public currency: string,
     public userId: string,
     public createdAt: Date,
     public updatedAt: Date,
     public finishedAt: Date | null
   ) {}
-
-    // Método que evalúa si la meta ya se cumplió
-  public get isCompleted(): boolean {
-    return this.currentAmount >= this.totalAmount;
-  }
-
-    // Método que devuelva el porcentaje de avance (de 0 a 100).
-  public get progressPercentage(): number {
-    if (this.totalAmount === 0) return 0;
-      const percentage = (this.currentAmount / this.totalAmount) * 100;
-      return Math.round(percentage);
-    }
 }

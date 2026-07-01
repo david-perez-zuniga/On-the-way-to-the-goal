2388f83b54caf048bc22e80730809e2589392a1a
destripador2000 <davidhumbertop503@gmail.com>
2026-07-01 16:59:30 -0600

    Feat: Implementación de la ruta DELETE para eliminar meta

--- backend/src/infrastructure/routes/goalRoutes.ts
+++ backend/src/infrastructure/routes/goalRoutes.ts
@@ -2,6 +2,7 @@ import { Router } from 'express';
 import { PrismaGoalRepository } from '../repositories/PrismaGoalRepository';
 import { CreateGoalUseCase } from '../../application/use-cases/CreateGoalUseCase';
 import { UpdateGoalUseCase } from '../../application/use-cases/UpdateGoalUseCase';
+import { DeleteGoalUseCase } from '../../application/use-cases/DeleteGoalUseCase';
 import { GoalController } from '../controllers/GoalController';
 
 const router : Router = Router();
@@ -10,10 +11,12 @@ const goalRepository = new PrismaGoalRepository();
 
 const createGoalUseCase = new CreateGoalUseCase(goalRepository);
 const updateGoalUseCase = new UpdateGoalUseCase(goalRepository);
+const deleteGoalUseCase = new DeleteGoalUseCase(goalRepository);
 
-const goalController = new GoalController(createGoalUseCase, updateGoalUseCase);
+const goalController = new GoalController(createGoalUseCase, updateGoalUseCase, deleteGoalUseCase);
 
 router.post('/', goalController.createGoal);
 router.put('/:id', goalController.updateGoal);
+router.delete('/:id', goalController.deleteGoal);
 
 export { router as goalRoutes };

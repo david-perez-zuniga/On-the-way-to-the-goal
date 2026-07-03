6d9bafc2b098691f04954257a5919aa8bf749d77
destripador2000 <davidhumbertop503@gmail.com>
2026-07-02 20:29:52 -0600

    Fix: Corrigiendo nombres de atributos, en ves de amount es deposit y en vez de createAt es depositDate

--- backend/src/domain/entities/Payment.ts
+++ backend/src/domain/entities/Payment.ts
@@ -3,12 +3,12 @@ import type { Prisma } from "../../infrastructure/db";
 export class Payment {
   constructor(
     public readonly id: string,
-    public readonly amount: Prisma.Decimal,
-    public readonly createdAt: Date,
+    public readonly deposit: Prisma.Decimal,
+    public readonly depositDate: Date,
     public readonly goalId: string,
   )
   {
-    if (amount.lte(0)) {
+    if (deposit.lte(0)) {
       throw new Error('El monto del abono debe ser mayor a cero');
     }
   }

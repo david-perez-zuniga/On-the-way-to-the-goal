3c6139ca11ef86184d852696c4c991b1d1fb8daf
destripador2000 <davidhumbertop503@gmail.com>
2026-07-04 05:48:08 -0600

    Feat: Agregando ruta de payment al index

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,6 +3,7 @@ import express from 'express';
 import { goalRoutes } from './infrastructure/routes/goalRoutes';
 import { userRoutes } from './infrastructure/routes/userRoutes';
 import { loginRoutes } from './infrastructure/routes/loginRoutes';
+import { paymentRoutes } from './infrastructure/routes/paymentRoutes';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -13,6 +14,7 @@ app.use(express.json());
 app.use('/api/goals', goalRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/login', loginRoutes)
+app.use('/api/payment', paymentRoutes)
 
 app.get('/api/health', (req, res) => {
   res.json({ status: 'ok', message: '¡Servidor de Camino a la Meta funcionando!' });

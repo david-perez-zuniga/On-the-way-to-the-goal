fd6999daf75ef2fad46eb3eeac2350c28a0eb3f1
destripador2000 <davidhumbertop503@gmail.com>
2026-07-09 22:23:05 -0600

    Feat: Creación de contrlador para obtener el historial de abonos

--- backend/src/infrastructure/controllers/PaymentController.ts
+++ backend/src/infrastructure/controllers/PaymentController.ts
@@ -1,9 +1,11 @@
 import { type Request, type Response } from "express";
 import { CreatePaymentUseCase } from "../../application/use-cases/CreatePaymentUseCase";
+import type { GetPaymentHistoryUseCase } from "../../application/use-cases/GetPaymentHistoryUseCase";
 
 export class PaymentController {
   constructor(
-    private readonly createPaymentUseCase: CreatePaymentUseCase
+    private readonly createPaymentUseCase: CreatePaymentUseCase,
+    private readonly getPaymentHistoryUseCase: GetPaymentHistoryUseCase
   ){}
 
   public createPayment = async (req: Request, res: Response): Promise<void> => {
@@ -16,4 +18,16 @@ export class PaymentController {
       res.status(500).json({error: 'Error interno del servidor al crear un deposito' })
     }
   };
+
+  public getHistory = async(req: Request, res: Response): Promise<void> => {
+    try{
+      const {goalId} = req.params as {goalId: string};
+      const history = await this.getPaymentHistoryUseCase.execute({ goalId })
+      res.status(200).json(history)
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({error: 'Error interno dle servidor al obttener historial'})
+    }
+  };
+  
 }

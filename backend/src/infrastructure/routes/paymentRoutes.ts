8009825129f638d9e981f43cdb6d29e02424c80f
destripador2000 <davidhumbertop503@gmail.com>
2026-07-09 22:18:21 -0600

    Feat: Agregando ruta para el historial de pagos en Payment

--- backend/src/infrastructure/routes/paymentRoutes.ts
+++ backend/src/infrastructure/routes/paymentRoutes.ts
@@ -3,6 +3,7 @@ import { PrismaPaymentRepository } from "../repositories/PrismaPaymentRepository
 import { CreatePaymentUseCase } from "../../application/use-cases/CreatePaymentUseCase";
 import { PaymentController } from "../controllers/PaymentController";
 import { PrismaGoalRepository } from "../repositories/PrismaGoalRepository";
+import { GetPaymentHistoryUseCase } from "../../application/use-cases/GetPaymentHistoryUseCase";
 
 const router : Router = Router();
 const goalRepository = new PrismaGoalRepository();
@@ -10,8 +11,12 @@ const goalRepository = new PrismaGoalRepository();
 
 const paymentRepository = new PrismaPaymentRepository();
 const createPaymentUseCase = new CreatePaymentUseCase(paymentRepository, goalRepository);
-const paymentController = new PaymentController(createPaymentUseCase);
+const getPaymentHistoryUseCase = new GetPaymentHistoryUseCase(paymentRepository)
 
+const paymentController = new PaymentController(createPaymentUseCase,
+                                                getPaymentHistoryUseCase);
+
+router.get('/goal/:goalId', paymentController.getHistory);
 router.post('/', paymentController.createPayment);
 
 export { router as paymentRoutes };

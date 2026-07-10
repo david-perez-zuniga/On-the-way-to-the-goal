e8a4bfaedeeb65b3fb8c82dc1b482edf77780f4b
destripador2000 <davidhumbertop503@gmail.com>
2026-07-09 21:44:00 -0600

    Feat: Agregando ruta para GetGoalProgress al router

--- backend/src/infrastructure/routes/paymentRoutes.ts
+++ backend/src/infrastructure/routes/paymentRoutes.ts
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { PrismaPaymentRepository } from "../repositories/PrismaPaymentRepository";
 import { CreatePaymentUseCase } from "../../application/use-cases/CreatePaymentUseCase";
 import { PaymentController } from "../controllers/PaymentController";
+import { PrismaGoalRepository } from "../repositories/PrismaGoalRepository";
 
 const router : Router = Router();
+const goalRepository = new PrismaGoalRepository();
+
 
 const paymentRepository = new PrismaPaymentRepository();
-const createPaymentUseCase = new CreatePaymentUseCase(paymentRepository);
+const createPaymentUseCase = new CreatePaymentUseCase(paymentRepository, goalRepository);
 const paymentController = new PaymentController(createPaymentUseCase);
 
 router.post('/', paymentController.createPayment);

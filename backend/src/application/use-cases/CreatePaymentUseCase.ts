07c5ec103f1199ce55f8c78a434bedcdc74ec208
destripador2000 <davidhumbertop503@gmail.com>
2026-07-10 17:36:09 -0600

    Feat: Agregando lógica de conversión de moneda en creación de pagos

--- backend/src/application/use-cases/CreatePaymentUseCase.ts
+++ backend/src/application/use-cases/CreatePaymentUseCase.ts
@@ -14,19 +14,30 @@ export class CreatePaymentUseCase {
               private readonly goalRepository: IGoalRepository) {}
 
   public async execute(data: CreatePaymentDTO): Promise<Payment> {
-    const depositDate = new Date()
+    const depositDate = new Date();
     const id = crypto.randomUUID();
+    let finalDeposit: Prisma.Decimal;
     const goal = await this.goalRepository.findById(data.goalId)
 
     if (goal == null){
       throw new Error('No se encontró la meta')
     }
 
+    if (data.currency === goal.currency){
+      finalDeposit = new Prisma.Decimal(data.deposit);
+    }
+    else if(data.currency === "NIO" && goal.currency === "USD"){
+      finalDeposit = new Prisma.Decimal(data.deposit).div(36.6)
+    }
+    else {
+      finalDeposit = new Prisma.Decimal(data.deposit).mul(36.6)
+    }
+
     const newPayment = new Payment(
       id,
-      new Prisma.Decimal(data.deposit),
+      finalDeposit,
       depositDate,
-      data.currency,
+      goal.currency,
       data.goalId
     )
     
